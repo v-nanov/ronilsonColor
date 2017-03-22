08b4ed61df672b52b861344681955fb1a6e56bd5
Ronilson <ronilsondaconceicao@gmail.com>
2017-03-22 11:53:39 -0300

    Initial Commit

---
+++ CorRquestJson/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CorRquestJson
+//
+//  Created by Ronilson on 22/03/17.
+//  Copyright © 2017 Ronilson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
